ebf268646846160f05a0b9deb3078b7229172ea5
yy736044583 <281375138@qq.com>
2018-01-03 14:38:40 +0800

    增加评价导出excel

--- application/admin/controller/Evaluate.php
+++ application/admin/controller/Evaluate.php
@@ -4,6 +4,7 @@ use think\Controller;
 use think\View;
 use think\Db;
 use app\admin\Controller\Common;
+use think\Loader;
 //评价管理  报表查询
 
 
@@ -47,8 +48,10 @@ class Evaluate extends Common{
 		}
 		$list = $data->all();
 		foreach ($list as $k => $v) {
+			$workman = Db::name('sys_workman')->where('id',$v['workmanid'])->field('name,sectionid')->find();
 			//员工姓名
-			$list[$k]['workmanid']	 = Db::name('sys_workman')->where('id',$v['workmanid'])->value('name');
+			$list[$k]['workmanid']	 = $workman['name'];
+			$list[$k]['section']	 = Db::name('sys_section')->where('id',$workman['sectionid'])->value('name');
 			$list[$k]['deviceid']	 = Db::name('pj_device')->where('id',$v['deviceid'])->value('number');
 			//评价
 			switch ($v['evaluatelevel']) {
@@ -81,7 +84,7 @@ class Evaluate extends Common{
 	//评价统计
 	public function count(){
 		$this->auth();
-		$data = Db::name('sys_workman')->field('id,name')->paginate(12);
+		$data = Db::name('sys_workman')->field('id,name,sectionid')->paginate(12);
 		$list = $data->all();
 		foreach ($list as $k => $v) {
 			$map1['workmanid'] = $v['id'];
@@ -107,6 +110,8 @@ class Evaluate extends Common{
 			}else{
 				$list[$k]['many'] = '0.00';
 			}
+
+			$list[$k]['section'] = Db::name('sys_section')->where('id',$v['sectionid'])->value('name');
 			
 		}		
 		$page = $data->render();
@@ -130,7 +135,7 @@ class Evaluate extends Common{
 		if($name!=''){//根据姓名查询员工id
 			$map['name'] = array('like',"%$name%");
 		}
-		$data = Db::name('sys_workman')->where($map)->paginate(12);
+		$data = Db::name('sys_workman')->field('id,name,sectionid')->where($map)->paginate(12);
 		$list = $data->all();
 		foreach ($list as $k => $v) {
 			//判断时间
@@ -169,6 +174,8 @@ class Evaluate extends Common{
 			}else{
 				$list[$k]['many'] = '0.00';
 			}
+
+			$list[$k]['section'] = Db::name('sys_section')->where('id',$v['sectionid'])->value('name');
 			
 		}		
 		$page = $data->render();
@@ -218,4 +225,174 @@ class Evaluate extends Common{
 		return $this->fetch();
 	}
 
+	public function downindex(){
+		return $this->fetch();
+	}
+
+
+	public function download(){
+		$start = input('start');
+		$end = input('end');
+		$map = array();
+		if($start!=''&$end==''){
+			$map1['evaluatetime'] = array('egt',$start);
+		}elseif($start==''&$end!=''){
+			$map1['evaluatetime'] = array('elt',$end);
+		}elseif($start!=''&$end!=''){
+			$map1['evaluatetime'] = array('between time',array($start,$end));
+		}
+    	// 实例化phpexcel
+    	Loader::import('first.PHPExcel');
+		$excel = new \PHPExcel();
+		// 根据事项id查询所有数据
+		$list = Db::name('pj_evaluate')->where($map)->select();
+		foreach ($list as $k => $v) {
+			$list[$k]['device'] = Db::name('pj_device')->where('id',$v['deviceid'])->value('number');
+			$workman = Db::name('sys_workman')->where('id',$v['workmanid'])->field('name,sectionid')->find();
+			//员工姓名
+			$list[$k]['workman']	 = $workman['name'];
+			$list[$k]['section'] = Db::name('sys_section')->where('id',$workman['sectionid'])->value('name');
+			switch ($v['evaluatelevel']) {
+				case '0':$list[$k]['evaluatelevel'] = '态度不好';break;
+				case '1':$list[$k]['evaluatelevel'] = '业务不熟';break;
+				case '2':$list[$k]['evaluatelevel'] = '时间太长';break;
+				case '3':$list[$k]['evaluatelevel'] = '有待改进';break;
+				case '4':$list[$k]['evaluatelevel'] = '基本满意';break;
+				case '5':$list[$k]['evaluatelevel'] = '非常满意';break;	
+				default:break;
+			}
+		}
+
+
+		//设置第一行的标题   可以把execl表打开看一下
+        $excel->setActiveSheetIndex(0)
+            ->setCellValue('A1','员工')
+            ->setCellValue('B1','部门')
+            ->setCellValue('C1','设备')
+            ->setCellValue('D1','评价')
+            ->setCellValue('E1','评价时间');
+            $i = 2;
+
+            // 设置行高
+            $excel->getActiveSheet()->getRowDimension('1')->setRowHeight(22); 
+            // 设置宽度
+            $excel->getActiveSheet()->getColumnDimension('A')->setWidth(20);  
+
+            // 遍历数据库查询的数据 放入excel
+            foreach ($list as $k => $v) {
+            	$excel->setActiveSheetIndex(0)
+                ->setCellValue('A'.$i,$v['workman'])
+                ->setCellValue('B'.$i,$v['section'])
+                ->setCellValue('C'.$i,$v['device'])
+                ->setCellValue('D'.$i,$v['evaluatelevel'])
+                ->setCellValue('E'.$i,$v['evaluatetime']);
+                $i++;
+            }
+        // 底部名称    
+  		$excel->getActiveSheet()->setTitle('评价记录');
+        $excel->setActiveSheetIndex(0);
+        $filename=urlencode('评价记录').'_'.date('Y-m-dHi');
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="'.$filename.'.xls"');
+        header('Cache-Control: max-age=0');
+
+        $objWriter = \PHPExcel_IOFactory::createWriter($excel, 'Excel5');  
+        $objWriter->save('php://output');
+        exit;
+	}
+
+	public function downindex1(){
+		return $this->fetch();
+	}
+
+	public function download1(){
+		$start = input('start');
+		$end = input('end');
+		$map = array();
+
+		$list = Db::name('sys_workman')->field('name,id,sectionid')->select();
+		foreach ($list as $k => $v) {
+			//判断时间
+			if($start!=''&$end==''){
+				$map1['evaluatetime'] = array('gt',$start);
+			}elseif($start==''&$end!=''){
+				$map1['evaluatetime'] = array('lt',$end);
+			}elseif($start!=''&$end!=''){
+				$map1['evaluatetime'] = array('between time',array($start,$end));
+			}
+
+			$map1['evaluatestatus'] = '1';
+			$map1['workmanid'] = $v['id'];
+			$map1['evaluatelevel'] = '0';
+			$list[$k]['evaluatelevel1']	= Db::name('pj_evaluate')->where($map1)->count();
+			$map1['evaluatelevel'] = '1';
+			$list[$k]['evaluatelevel2']	= Db::name('pj_evaluate')->where($map1)->count();
+			$map1['evaluatelevel'] = '2';
+			$list[$k]['evaluatelevel3']	= Db::name('pj_evaluate')->where($map1)->count();
+			$map1['evaluatelevel'] = '3';
+			$list[$k]['evaluatelevel4']	= Db::name('pj_evaluate')->where($map1)->count();
+			$map1['evaluatelevel'] = '4';
+			$list[$k]['evaluatelevel5']	= Db::name('pj_evaluate')->where($map1)->count();
+			$map1['evaluatelevel'] = '5';
+			$list[$k]['evaluatelevel6']	= Db::name('pj_evaluate')->where($map1)->count();
+			//小计
+			$list[$k]['sum'] = 	$list[$k]['evaluatelevel1']+$list[$k]['evaluatelevel2']+$list[$k]['evaluatelevel3']+$list[$k]['evaluatelevel4']+$list[$k]['evaluatelevel5']+$list[$k]['evaluatelevel6'];
+			//满意率
+			$many = $list[$k]['evaluatelevel6']+$list[$k]['evaluatelevel5'];
+			if($many!=0){
+				$list[$k]['many'] = round(($many/$list[$k]['sum']),2);
+			}else{
+				$list[$k]['many'] = '0.00';
+			}
+			$list[$k]['section'] = Db::name('sys_section')->where('id',$v['sectionid'])->value('name');
+			
+		}
+    	// 实例化phpexcel
+    	Loader::import('first.PHPExcel');
+		$excel = new \PHPExcel();
+
+		//设置第一行的标题   可以把execl表打开看一下
+        $excel->setActiveSheetIndex(0)
+            ->setCellValue('A1','员工')
+            ->setCellValue('B1','非常满意')
+            ->setCellValue('C1','基本满意')
+            ->setCellValue('D1','有待改进')
+            ->setCellValue('E1','时间太长')
+            ->setCellValue('F1','业务不熟')
+            ->setCellValue('G1','态度不好')
+            ->setCellValue('H1','满意率')
+            ->setCellValue('I1','小计');
+            ->setCellValue('J1','部门');
+            $i = 2;
+
+            // 设置行高
+            $excel->getActiveSheet()->getRowDimension('1')->setRowHeight(22);   
+
+            // 遍历数据库查询的数据 放入excel
+            foreach ($list as $k => $v) {
+            	$excel->setActiveSheetIndex(0)
+                ->setCellValue('A'.$i,$v['name'])
+                ->setCellValue('B'.$i,$v['evaluatelevel6'])
+                ->setCellValue('C'.$i,$v['evaluatelevel5'])
+                ->setCellValue('D'.$i,$v['evaluatelevel4'])
+                ->setCellValue('E'.$i,$v['evaluatelevel3'])
+                ->setCellValue('F'.$i,$v['evaluatelevel2'])
+                ->setCellValue('G'.$i,$v['evaluatelevel1'])
+                ->setCellValue('H'.$i,$v['many'].'%')
+                ->setCellValue('I'.$i,$v['sum']);
+                ->setCellValue('J'.$i,$v['section']);
+                $i++;
+            }
+        // 底部名称    
+  		$excel->getActiveSheet()->setTitle('评价统计');
+        $excel->setActiveSheetIndex(0);
+        $filename=urlencode('评价统计').'_'.date('Y-m-dHi');
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="'.$filename.'.xls"');
+        header('Cache-Control: max-age=0');
+
+        $objWriter = \PHPExcel_IOFactory::createWriter($excel, 'Excel5');  
+        $objWriter->save('php://output');
+        exit;
+	}
 }
